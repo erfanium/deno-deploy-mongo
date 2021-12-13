8f00a8b65f41ca2a57e9dab35e413028d5fa3724
ERFANIUM <erfanshield@outlook.com>
2021-12-13 19:43:33 +0330

    add eta

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ import {
   Status,
 } from "https://deno.land/x/oak@v10.0.0/mod.ts";
 import { Bson, MongoClient } from "https://deno.land/x/mongo@v0.28.1/mod.ts";
+import * as eta from "https://deno.land/x/eta@v1.12.3/mod.ts";
 
 const MONGO_URI = Deno.env.get("MONGO_URI");
 if (!MONGO_URI) throw new Error("MONGO_URI not found");
@@ -16,11 +17,21 @@ const client = new MongoClient();
 await client.connect(MONGO_URI);
 const collection = client.database().collection("posts");
 
+const viewPath = `${Deno.cwd()}/views/`;
+
+eta.configure({
+  views: viewPath,
+});
+
 router.get("/", async (ctx) => {
   const posts = await collection.find({}, { noCursorTimeout: false })
     .toArray();
 
   ctx.response.body = posts;
+});
+
+router.get("/test", async (ctx) => {
+  ctx.response.body = await eta.renderFile("./posts", { food: "cake" })!;
 });
 
 router.post("/", async (ctx) => {
